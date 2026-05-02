350468e76bd59ac81b6980acba35987d5a2b5fef
Barbara Tobares <barbara.tobares@gmail.com>
2026-05-02 18:54:53 -0300

    Modificaciones del template - se sacan los circulos, se cambia el visual de las cajas

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,13 @@
 #include <box2d.h>
 #include <vector>
 
+using namespace std;
+
+//escala para convertir pixeles/metros
+//metros a píxeles : *SCALE
+//píxeles a metros : / SCALE
+const float SCALE = 30.0f;
+
 struct PhysicsBox
 {
     b2Body* body;
@@ -19,7 +26,7 @@ int main(void)
     const int screenWidth = 1000;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "MAVI II - Bienvenida");
+    InitWindow(screenWidth, screenHeight, "Las cajas de Mavix");
     SetTargetFPS(60);
 
     Color fondo = { 110, 100, 215, 255 };
@@ -27,7 +34,7 @@ int main(void)
     Color textoSecundario = DARKPURPLE;
     Color sueloColor = Fade(DARKGREEN, 0.7f);
 
-    // Mundo físico
+    // Mundo físico - todo se crea DIVIDIENDO el tamaño por scale
     b2Vec2 gravity(0.0f, 9.8f);
     b2World world(gravity);
 
@@ -36,14 +43,28 @@ int main(void)
     // -----------------------------
     b2BodyDef groundDef;
     groundDef.type = b2_staticBody;
-    groundDef.position.Set(screenWidth / 2.0f, screenHeight - 40.0f);
-    b2Body* groundBody = world.CreateBody(&groundDef);
+    groundDef.position.Set(
+        (screenWidth / 2.0f) / SCALE,
+        (screenHeight - 40.0f) / SCALE
+    );
 
     b2PolygonShape groundShape;
-    groundShape.SetAsBox(screenWidth / 2.0f, 20.0f);
+    groundShape.SetAsBox(
+        (screenWidth / 2.0f) / SCALE,
+        20.0f / SCALE
+    );
+
+    b2Body* groundBody = world.CreateBody(&groundDef);
+
     groundBody->CreateFixture(&groundShape, 0.0f);
 
-    std::vector<PhysicsBox> boxes;
+    /*
+    b2Body = objeto lógico (como una entidad)
+    b2Shape = la geometría de esa parte
+    b2Fixture = “parte física tangible”
+    */
+
+    vector<PhysicsBox> boxes;
 
     // -----------------------------
     // Crear algunas cajas dinámicas
@@ -52,12 +73,18 @@ int main(void)
     {
         b2BodyDef boxDef;
         boxDef.type = b2_dynamicBody;
-        boxDef.position.Set(300.0f + i * 80.0f, 80.0f + i * 20.0f);
-
-        b2Body* boxBody = world.CreateBody(&boxDef);
+        boxDef.position.Set(
+            (300.0f + i * 80.0f) / SCALE,
+            (80.0f + i * 20.0f) / SCALE
+        );
 
         b2PolygonShape boxShape;
-        boxShape.SetAsBox(25.0f, 25.0f);
+        boxShape.SetAsBox(
+            (25.0f) / SCALE,
+            (25.0f) / SCALE
+        );
+
+        b2Body* boxBody = world.CreateBody(&boxDef);
 
         b2FixtureDef boxFixture;
         boxFixture.shape = &boxShape;
@@ -72,7 +99,7 @@ int main(void)
 
     while (!WindowShouldClose())
     {
-        // Avanzar simulación
+        // Avanzar simulación - sincronizado con el juego a 60fps (definido mas arriba)
         world.Step(1.0f / 60.0f, 8, 3);
 
         BeginDrawing();
@@ -81,22 +108,22 @@ int main(void)
         // Suelo visual - rectangulo que no tiene fisicas, es solo una imagen
         DrawRectangle(0, screenHeight - 60, screenWidth, 40, sueloColor);
 
-        // Dibujar cajas
+        // Dibujar cajas - aca paso de metros a pixeles MULTIPLICANDO el tamaño por scale
         for (const auto& box : boxes)
         {
             b2Vec2 pos = box.body->GetPosition();
             float angle = box.body->GetAngle() * RAD2DEG;
 
             Rectangle rect = {
-                pos.x - box.width / 2.0f,
-                pos.y - box.height / 2.0f,
+                (pos.x * SCALE) - box.width / 2.0f,
+                (pos.y * SCALE) - box.height / 2.0f + 30.0f,
                 box.width,
                 box.height
             };
 
             Vector2 origin = { box.width / 2.0f, box.height / 2.0f };
             DrawRectanglePro(rect, origin, angle, box.color);
-            DrawRectangleLinesEx(rect, 2, DARKBLUE);
+            //DrawRectangleLinesEx(rect, 2, DARKBLUE);
         }
 
         DrawRectangle(90, 70, 820, 90, Fade(BLACK, 0.18f));
